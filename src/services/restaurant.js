82bd879f4615564474dbeffeff341e24afaede7e
shaheer924 <39200747+shaheer924@users.noreply.github.com>
2023-11-21 22:33:48 +0500

    work

---
+++ src/services/restaurant.js
@@ -0,0 +1,5 @@
+import { apiUrl } from '../utils/constants'
+import api from './index'
+
+export const getRestaurant = async (params) => api.get(`restaurant`,{params})
+export const getRestaurantbyId = async (id) => api.get(`${apiUrl.restaurant}/${id}`)
